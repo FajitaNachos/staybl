f6c615c8473f4417bd800b7a101716bd76144cf3
FajitaNachos <andrew@fajitanachos.com>
2013-05-20 22:12:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staybl::Application.config.secret_token = '3670a6061f34f2b1dc9c7ad2fba71dcfed43d29ed3c69b681fe861b8aa68fce93449ace85efcb930d3d21803d5860fe6abe397873aab5568a4250d1c5c9c865b'
