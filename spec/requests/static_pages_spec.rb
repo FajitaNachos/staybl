7738ce1ebce0dd039ec0d37566c0b9f63c2bdd50
FajitaNachos <andrew@fajitanachos.com>
2013-05-23 07:32:49 +0900

    Refactor site title and tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,42 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:site_title) {"Stabyl"}
+
   describe "Home page" do
 
-    it "should have the content 'Staybl'" do
+    it "should not have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_content('Staybl')
+      page.should_not have_title('Home')
     end
-  end
 
-  describe "Contact page" do
-
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_content('Contact')
-    end
-  end
-
-  describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_title('Staybl')
     end
   end
 
   describe "Privacy Policy" do
 
-    it "should have the content 'Privacy Policy'" do
+    it "should have the title 'Privacy'" do
       visit '/static_pages/privacy'
-      page.should have_content('Privacy Policy')
+      page.should have_title('Privacy')
     end
   end
 
+  describe "About page" do
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_title('About')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_title('Contact')
+    end
+  end
 end
