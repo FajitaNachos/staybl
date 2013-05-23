c79c7fb5862ed1eaa53060af22f561849c8833a3
FajitaNachos <andrew@fajitanachos.com>
2013-05-23 18:01:35 +0900

    Remove test/ and added features/

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+ 
+  let(:base_title) { "Stabyl" } 
+
+  describe "Home page" do 
+ 
+    it "should not have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should_not have_title('Home') 
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'  
+      page.should have_title("Staybl")
+    end
+  end
+
+  describe "Privacy Policy" do
+
+    it "should have the title 'Privacy'" do
+      visit '/static_pages/privacy'
+      page.should have_title("Staybl | Privacy")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_title('Staybl | About')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_title('Staybl | Contact')
+    end
+  end
+end
