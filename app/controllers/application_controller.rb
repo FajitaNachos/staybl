ada8ce3d6786b50f5cffc112d5b7db08e590aa10
FajitaNachos <andrew@fajitanachos.com>
2013-07-11 08:19:20 +0900

    Remove anonymous voting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery
+  
 
   def index
-  end
+  end  
 
- 
-  
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 end
